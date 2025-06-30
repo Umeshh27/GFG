621e31da4a153fbf69f3349c0ed6e1202e50a735
Alla Umesh <23MH1A05E7@acoe.edu.in>
2025-06-30 17:34:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Linked List/print-linked-list.java
@@ -0,0 +1,23 @@
+/* Node is defined as
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+/*
+    Print elements of a linked list on console
+    Head pointer input could be NULL as well for empty list
+*/
+
+class Solution {
+    // Function to display the elements of a linked list in same line
+    void printList(Node head) {
+      while(head!=null){
+          System.out.print(head.data + " ");
+          head=head.next;
+      }
+    }
+}
